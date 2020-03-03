df6c4f886d3aa938175e3364ccc2ff48ad9cd873
c00kamunga <tsothalanti2@yahoo.com>
2020-03-03 15:21:52 -0800

    added like functionality

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -1,10 +1,11 @@
 // look at the likes on this component. Right now it is hard coded on line 20.
 // console.log your props and see how to add likes to this component so that it will update when you click the icon.
 // HINT: You will need to add an onClick event pass through your props and update the hard coded Likes
-import React from "react";
+import React, {useState} from "react";
 
 const LikeSection = (props) => {
   console.log(props)
+  const [likes, setLikes] = useState(0);
   return (
     <div>
     <div
@@ -18,7 +19,10 @@ const LikeSection = (props) => {
         <i className="far fa-comment" />
       </div>
     </div>
-    <p className="like-number">likes</p>
+  <p className="like-number">{likes}</p>
+  <button onClick={() => setLikes(likes + 1)}>
+    Like
+  </button>
 </div>
   )
 };
