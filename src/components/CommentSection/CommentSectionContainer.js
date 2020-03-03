e91c60d10a3495f6787371b324ce8e7682fb53db
c00kamunga <tsothalanti2@yahoo.com>
2020-03-03 15:06:04 -0800

    console.log(props) got data back, edited map method, getting cannot read map property of undefined

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -3,17 +3,18 @@ import React, { useState } from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
-import dummyData from "../../dummy-data";
+
 
 
 const CommentSection = props => {
   const [comments] = useState();
+  console.log(props)
   // Add state for the comments
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
-      {dummyData.comments.map(item => {
+      {props.comment.map((item) => {
         return <Comment comment={item}/>
       })}
      <CommentInput/>;
